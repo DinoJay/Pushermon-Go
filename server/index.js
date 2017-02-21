20a923ee8134dee99ee30ccadf5c319dff8c7a9c
DinoJay <jott88@googlemail.com>
2017-02-21 19:42:30 +0100

    added simple cards to map interface

--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
-const Hapi = require('hapi');
+const Hapi = require("hapi");
 const server = new Hapi.Server();
-const fetch = require('node-fetch');
-const ngeohash = require('ngeohash');
-const utils = require('./utils');
-const Pusher = require('pusher');
-const fs = require('fs');
-require('dotenv').config();
+const fetch = require("node-fetch");
+const ngeohash = require("ngeohash");
+const utils = require("./utils");
+const Pusher = require("pusher");
+const fs = require("fs");
+require("dotenv").config();
 
 const pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID,
   key: process.env.PUSHER_APP_KEY,
   secret: process.env.PUSHER_APP_SECRET,
-  cluster: 'eu',
+  cluster: "eu",
   encrypted: true
 });
 
-console.log('Pusher', process.env.PUSHER_APP_ID);
+console.log("Pusher", process.env.PUSHER_APP_ID);
 // List of channels that have users subscribed to
 const channels = new Set();
 
@@ -29,32 +29,19 @@ server.connection({
   // tls
 });
 
-server.register([require('inert'), require('vision')], (err) => {
+server.register([require("inert"), require("vision")], (err) => {
   if (err) {
     throw err;
   }
 
-  server.views({
-    engines: {
-      html: require('handlebars')
-    },
-    context: {
-      mapboxAccessToken: process.env.MAPBOX_ACCESS_TOKEN,
-      pusherAccessToken: process.env.PUSHER_APP_KEY,
-      pusherCluster: 'eu'
-    },
-    relativeTo: __dirname,
-    path: 'templates'
-  });
-
   server.route({
-    method: 'POST',
-    path: '/channelhook',
+    method: "POST",
+    path: "/channelhook",
     handler (request, reply) {
-      console.log('channelhook');
-      console.log('handler');
-      console.log('handler');
-      console.log('handler', request.payload);
+      console.log("channelhook");
+      console.log("handler");
+      console.log("handler");
+      console.log("handler", request.payload);
 
       const webhook = pusher.webhook({
         rawBody: JSON.stringify(request.payload),
@@ -62,17 +49,17 @@ server.register([require('inert'), require('vision')], (err) => {
       });
       //
       if (!webhook.isValid()) {
-        console.log('Invalid webhook');
+        console.log("Invalid webhook");
         return reply(400);
       } else {
         reply(200);
       }
 
       webhook.getEvents().forEach((e) => {
-        if (e.name == 'channel_occupied') {
+        if (e.name == "channel_occupied") {
           channels.add(e.channel);
         }
-        if (e.name == 'channel_vacated') {
+        if (e.name == "channel_vacated") {
           channels.delete(e.channel);
         }
       });
@@ -89,11 +76,11 @@ server.register([require('inert'), require('vision')], (err) => {
 
   // Static resources
   server.route({
-    method: 'GET',
-    path: '/{param*}',
+    method: "GET",
+    path: "/{param*}",
     handler: {
       directory: {
-        path: 'public'
+        path: "public"
       }
     }
   });
@@ -103,21 +90,21 @@ server.start((err) => {
   if (err) {
     throw err;
   }
-  console.log('Server running at:', server.info.uri);
+  console.log("Server running at:", server.info.uri);
 
   function nextEncounter() {
     const channelArray = Array.from(channels);
     const bounds = channelArray[Math.floor(Math.random() * channelArray.length)];
     if (bounds) {
-      console.log('nextEncounter', bounds);
+      console.log("nextEncounter", bounds);
       const boundingBox = ngeohash.decode_bbox(bounds);
       const lngMin = boundingBox[1];
       const lngMax = boundingBox[3];
       const latMin = boundingBox[0];
       const latMax = boundingBox[2];
 
-      const lng = utils.randomNumber(lngMin, lngMax).toFixed(10);
-      const lat = utils.randomNumber(latMin, latMax).toFixed(10);
+      const longitude = utils.randomNumber(lngMin, lngMax).toFixed(10);
+      const latitude = utils.randomNumber(latMin, latMax).toFixed(10);
       const duration = utils.randomNumber(30, 300) * 1000;
 
       const pokemonId = parseInt(utils.randomNumber(1, 250), 10);
@@ -125,17 +112,17 @@ server.start((err) => {
       fetch(`http://pokeapi.co/api/v2/pokemon/${pokemonId}/`)
         .then(res => res.json())
         .then((pokemon) => {
-          console.log('pokemon', pokemon);
+          console.log("pokemon", pokemon);
           const data = {
             id: pokemonId,
             name: pokemon.name,
             sprite: `https://pokeapi.co/media/sprites/pokemon/${pokemonId}.png`,
-            coordinates: [lng, lat],
+            coords: {latitude, longitude},
             expires: parseInt((new Date()).getTime() + duration, 10),
-            hp: pokemon.stats.find(stat => stat.stat.name === 'hp').base_stat,
+            hp: pokemon.stats.find(stat => stat.stat.name === "hp").base_stat,
             types: pokemon.types.map(type => type.type.name[0] + type.type.name.substring(1))
           };
-          pusher.trigger(bounds, 'encounter', data);
+          pusher.trigger(bounds, "encounter", data);
         });
 
           // const data = {
